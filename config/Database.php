19580d9c6673f489404f7375f9a6e2875d128e9c
Uwakmfon1 <udohuwakmfon2@gmail.com>
2024-09-30 15:25:10 +0100

    added-database-class-feat

--- config/Database.php
+++ config/Database.php
@@ -1,19 +1,27 @@
-<?php 
+<?php
+class Database
+{
+    private $host = 'localhost';
 
-class Database{
-    
-private $conn;
+    private $dbname = 'laravelapi';
+
+    private $username = 'root';
+
+    private $password = '';
+
+    private $conn;
+
     public function connect()
     {
         $this->conn = null;
 
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // $this->conn = new PDO("mysql:host=" . $this->host . ';dbname=' . $this->dbname. 'charset=utf8', $this->username, $this->password);
+            $this->conn = new PDO("mysql:host= $this->host;dbname= $this->dbname;charset=utf8",  $this->username,  $this->password);
         } catch (PDOException $e) {
             echo 'connection Error: ' . $e->getMessage();
         }
         return $this->conn;
-     
     }
 }
